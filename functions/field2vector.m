dd00575a48020c3d7498d478d59b9e2eaf776a4e
omidashtari <omid.ashtari@epfl.ch>
2024-03-19 10:27:32 +0100

    field-vector transformations added.

---
+++ functions/field2vector.m
@@ -0,0 +1,29 @@
+%%% DESCRIPTION -----------------------------------------------------------
+%   the minimal vector of state variables defining a 1D periodic field
+
+
+%%% INPUTS ----------------------------------------------------------------
+%   f       the field in physical state (column vector of real numbers)
+%   N       spatial resolution
+%   symm    center symmetry (true/false boolean)
+
+
+%%% OUTPUTS ---------------------------------------------------------------
+%   v       state vector (column vector of real numbers)
+
+
+%%% REMARKS ---------------------------------------------------------------
+%   1-  In forward integration of the KSE, the mean remains constant.
+%       We assume the mean value is always zero, hence not stored.
+
+
+function v = field2vector(f,N,symm)
+    Nd = round(N/3);
+    
+    F = fft(f);
+    v = imag(F(2:Nd+1));
+
+    if symm
+        v = [v;real(F(2:Nd+1))];
+    end
+end
---
+++ functions/vector2field.m
@@ -0,0 +1,36 @@
+%%% DESCRIPTION -----------------------------------------------------------
+%   1D periodic field corresponding to a minimal vector of state variables
+
+
+%%% INPUTS ----------------------------------------------------------------
+%   v       state vector (column vector of real numbers)
+%   N       spatial resolution
+%	symm    center symmetry (true/false boolean)
+
+
+%%% OUTPUTS ---------------------------------------------------------------
+%   f       the field in physical state (column vector of real numbers)
+
+
+%%% REMARKS ---------------------------------------------------------------
+%	1-  In forward integration of the KSE, the mean remains constant.
+%       We assume the mean value is always zero, hence the mean value of
+%       'f' is zero by construction.
+%   2-  We do not fill the padded elements in Fourier space, hence no
+%       additional dealiasing is needed.
+
+
+function f = vector2field(v,N,symm)
+    Nd = round(N/3);
+
+    F = zeros(N,1);
+    F(2:Nd+1) = v(1:Nd)*1j;
+    
+    if symm
+        F(2:Nd+1) = F(2:Nd+1) + v(Nd+1:2*Nd);
+    end
+
+    F(N-Nd+1:N) = flip(conj(F(2:Nd+1)));
+    
+    f = ifft(F,'symmetric');
+end
