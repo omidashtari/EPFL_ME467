39b42334efd886e47e12c4a42f6729dc7891f979
omidashtari <omid.ashtari@epfl.ch>
2024-03-26 09:18:58 +0100

    example programs 1 (time integration) and 2 (search for UPO) added.

---
+++ programs/example1.m
@@ -0,0 +1,24 @@
+close all; clear; clc
+
+addpath('../functions/')
+
+L = 39;
+N = 64;
+symm = true;
+T = 500;
+dt = 0.1;
+
+[x,~] = domain(L,N);
+u0 = sin(2*pi*x/L);
+
+v0 = field2vector(u0,N,symm);
+[vT,~] = KSE_integrate(v0,T,dt,0,L,N,symm);
+
+uT = vector2field(vT,N,symm);
+
+figure
+    plot(x,uT,'LineWidth',2)
+    hold on; grid on
+    plot(x,u0,'LineWidth',2)
+	xlabel('x'); ylabel('u')
+    legend('u(T)','u(0)')
---
+++ programs/example2.m
@@ -0,0 +1,17 @@
+close all; clear; clc
+
+addpath('../functions/')
+
+L = 39;
+N = 64;
+symm = true;
+T = 500;
+dt = 0.1;
+
+[x,~] = domain(L,N);
+u0 = sin(2*pi*x/L);
+
+u_guess = field2vector(u0,N,symm);
+T_guess = 50;
+
+[u_best,T_best] = search4PO(u_guess,T_guess,dt,L,N,symm); 
