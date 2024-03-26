a32576ab7cd871f94fcedfb98a574bed45a4b651
omidashtari <omid.ashtari@epfl.ch>
2024-03-26 15:21:51 +0100

    minor

--- functions/Jacobian.m
+++ functions/Jacobian.m
@@ -3,20 +3,20 @@
 
 
 %%% INPUTS ----------------------------------------------------------------
-%   u0      reference point of the Jacobian (column state vector)
-%   t       integration time interval
-%   eps     perturbation magnitude for finite difference derivatrives
-%   dt      step size in time integrations
-%   L       domain length
-%   N       spatial resolution
-%   symm	center symmetry (true/false boolean)
+%   u0          reference point of the Jacobian (column state vector)
+%   t           integration time interval
+%   epsilon     perturbation magnitude for finite difference derivatrives
+%   dt          step size in time integrations
+%   L           domain length
+%   N           spatial resolution
+%   symm        center symmetry (true/false boolean)
 
 
 %%% OUTPUTS ---------------------------------------------------------------
 %   J       Jacobian matrix
 
 
-function J = Jacobian(u0,t,eps,dt,L,N,symm)
+function J = Jacobian(u0,t,epsilon,dt,L,N,symm)
     n = length(u0);
     J = zeros(n,n);
     
